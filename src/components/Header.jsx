f5038a6ffea08ea3eda823c12256dc21f4be01e1
Kumar Akshat <kakshat247@gmail.com>
2024-01-30 10:25:53 +0530

    imporved the dark and light mode toggle button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,37 @@
-import React, { useContext } from 'react'
-import Switch from 'react-switch'
-import { AppContext } from '../context/AppContext'
+import React, { useContext } from "react";
+import Switch from "react-switch";
+import { AppContext } from "../context/AppContext";
+// import sun from '../../public/sun.png'
+import { FaMoon, FaSun } from "react-icons/fa";
 
 function Header() {
-
-  const { theme , setTheme , clickHandler } = useContext(AppContext);
+  const { theme, setTheme, clickHandler } = useContext(AppContext);
   return (
-    <div className='w-full drop-shadow-md' id={theme}>
-
-      <div className='w-full mx-auto flex items-center justify-evenly p-2 head'>
+    <div className="w-full drop-shadow-md" id={theme}>
+      <div className="w-full mx-auto flex items-center justify-evenly p-2 head">
         <header>
-            <h1 className='text-2xl md:text-3xl font-bold uppercase title'>Techy Blogz</h1>
+          <h1 className="text-2xl md:text-3xl font-bold uppercase title">
+            Techy Blogz
+          </h1>
         </header>
-        <div className='flex flex-row-reverse items-center gap-x-3'>
-          
-          <label htmlFor='togglebtn' className='togglebtnlabel'>{theme==='Light' ? 'Light Mode' : 'Dark Mode'}</label>
-          <Switch id='togglebtn' checked={theme==='Dark'} onChange={clickHandler}/>
-
+        <div className="flex flex-row-reverse items-center gap-x-3">
+          {/* <label htmlFor="togglebtn" className="togglebtnlabel">
+            {theme === "Light" ? "Light Mode" : "Dark Mode"}
+          </label> */}
+          <Switch
+          className="relative"
+            id="togglebtn"
+            checkedIcon={<FaSun className="text-yellow-300 text-lg absolute top-1 left-1" size={21} />}
+            uncheckedIcon={<FaMoon className="text-black text-lg absolute top-1 left-1" size={20} />}
+            offHandleColor="#ffee00"
+            onHandleColor="#2c2828"
+            checked={theme === "Dark"}
+            onChange={clickHandler}
+          />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
